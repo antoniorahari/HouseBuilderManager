286303ccd64fb4fd5daedeb43582103f5d050480
antoniorahari <50102671+antoniorahari@users.noreply.github.com>
2025-11-23 12:03:49 +0300

    Delete HouseBuilderManager directory

--- HouseBuilderManager/client/src/lib/currency.ts
+++
@@ -1,7 +0,0 @@
-export function formatCurrency(amount: number): string {
-  return new Intl.NumberFormat("fr-FR", {
-    style: "decimal",
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
-  }).format(amount);
-}
